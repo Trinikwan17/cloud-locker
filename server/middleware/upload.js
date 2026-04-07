3c95afc5cdddd242773fcf6ab01624b0eec4f8d8
Saukhya Patil <patilsaukhya97@gmail.com>
2026-04-08 01:27:39 +0530

    Update upload.js

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,21 +1,17 @@
 const multer = require('multer');
-const multerS3 = require('multer-s3');
-const s3Client = require('../config/s3Config');
+const path = require('path');
 
-// This is the variable that was missing!
-const upload = multer({
-  storage: multerS3({
-    s3: s3Client,
-    bucket: process.env.S3_BUCKET_NAME,
-    metadata: function (req, file, cb) {
-      cb(null, { fieldName: file.fieldname });
-    },
-    key: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-      cb(null, 'uploads/' + uniqueSuffix + '-' + file.originalname);
-    }
-  })
+// Define storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // folder in your server
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  }
 });
 
-// Now Node.js knows what to export
+const upload = multer({ storage });
+
 module.exports = upload;
